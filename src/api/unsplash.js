cea946f6ce8b0dbbf4b60a69ee719daa2498ff5d
ihsanfikri12 <fikriabiyyu@gmail.com>
2021-12-26 22:03:14 +0700

    fix small bug

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID YOURKEYHERE",
+    Authorization: "Client-ID YOURKEY",
   },
 });
 
